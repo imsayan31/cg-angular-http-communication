87e4c63ad061b677ae41007b3ca4299fe3826506
imsayan31 <sayantadeytraining@gmail.com>
2021-09-23 18:45:09 +0530

    Day 2: Intercepting Responses and Using Multiple Interceptors

--- src/app/core/add-header.interceptor.ts
+++ src/app/core/add-header.interceptor.ts
@@ -1,7 +1,10 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
+@Injectable()
 export class AddHeaderInterceptor implements HttpInterceptor{
+    
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
         console.log(`AddHeaderInterceptor called: ${req.url}`)
 
