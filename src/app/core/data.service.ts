ac7b7afe04b4f48247465a030bdbfcca125530fd
imsayan31 <sayantadeytraining@gmail.com>
2021-09-23 13:03:35 +0530

    Day 2: Http calls for getting list of books and book details

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { from, Observable } from 'rxjs';
+import {map, tap } from 'rxjs/operators';
 
 import { allBooks, allReaders } from 'app/data';
 import { Reader } from "app/models/reader";
 import { Book } from "app/models/book";
 import { BookTrackerError } from 'app/models/bookTrackerError';
+import { OldBook } from 'app/models/oldBook';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   mostPopularBook: Book = allBooks[0];
 
@@ -26,11 +30,32 @@ export class DataService {
     return allReaders.find(reader => reader.readerID === id);
   }
 
-  getAllBooks(): Book[] {
-    return allBooks;
+  getAllBooks(): Observable<Book[]> {
+    /* return allBooks; */
+    console.log('Getting all books from the server.')
+    return this.http.get<Book[]>('/api/books');
   }
 
-  getBookById(id: number): Book {
-    return allBooks.find(book => book.bookID === id);
-  }  
+  getBookById(id: number): Observable<Book> {
+    /* return allBooks.find(book => book.bookID === id); */
+    let getHeaders: HttpHeaders = new HttpHeaders({
+      'Accept': 'application/json',
+      'Authorization': 'my-token'
+    })
+    return this.http.get<Book>(`/api/books/${id}`, {
+      headers: getHeaders
+    })
+  }
+  
+  getOldBookById(id: number): Observable<OldBook>{
+    return this.http.get<Book>(`/api/books/${id}`)
+      .pipe(
+        map(b => <OldBook>{
+          bookTitle: b.title,
+          year: b.publicationYear
+        }),
+        tap(classicBook => console.log(classicBook))
+      );
+  }
+
 }
